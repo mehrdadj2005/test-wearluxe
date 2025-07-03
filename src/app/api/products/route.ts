e95689dbf24de9a5433113ba6b0c4436e43b8f31
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-07-03 14:50:04 +0330

    Enhance GET function to filter products by categoryId from request URL

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,15 +1,23 @@
-// app/api/products/route.ts
-
 import { NextResponse } from "next/server";
 import path from "path";
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
+    const url = new URL(request.url);
+    const categoryId = url.searchParams.get("categoryId");
+
     const filePath = path.join(process.cwd(), "data", "db.json");
     const file = await filePath;
     const json = JSON.parse(file);
 
-    return NextResponse.json(json.products);
+    let products = json.products;
+    if (categoryId) {
+      products = products.filter(
+        (p: any) => String(p.categoryId) === String(categoryId)
+      );
+    }
+
+    return NextResponse.json(products);
   } catch (error) {
     return NextResponse.json(
       { error: "خطا در خواندن اطلاعات محصولات" },
