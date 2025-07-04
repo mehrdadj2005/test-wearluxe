4b3fd49d5514850a1374c6cf95f861b5df8ffcbf
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-07-04 10:25:33 +0330

    Update HomePage component to switch API endpoints to local URLs and clean up commented code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,34 @@
-import Container from "@/components/container";
 import HeroSection from "@/components/layout/hero";
 import ProductSlider from "@/components/products/productSlider";
 import { getProduct } from "@/services/getProduct";
 import { IBanner } from "@/types/banners";
 import { IOption } from "@/types/options";
 import { IProduct } from "@/types/product";
-import { Box, CardMedia, Typography } from "@mui/material";
 
 export default async function HomePage() {
   const { data: dataShirts } = await getProduct<IProduct[]>(
-    "https://wearluxe-api.onrender.com/products?categoryId=1"
+    "http://localhost:4000/products?categoryId=1"
   );
   const { data: dataPants } = await getProduct<IProduct[]>(
-    "https://wearluxe-api.onrender.com/products?categoryId=2"
+    "http://localhost:4000/products?categoryId=2"
   );
   const { data: dataCaps } = await getProduct<IProduct[]>(
-    "https://wearluxe-api.onrender.com/products?categoryId=3"
+    "http://localhost:4000/products?categoryId=3"
   );
   const { data: dataSets } = await getProduct<IProduct[]>(
-    "https://wearluxe-api.onrender.com/products?categoryId=4"
+    "http://localhost:4000/products?categoryId=4"
   );
   const { data: dataOptions } = await getProduct<IOption[]>(
-    "https://wearluxe-api.onrender.com/options"
+    "http://localhost:4000/options"
   );
   const { data: landingBaner } = await getProduct<IBanner[]>(
-    "https://wearluxe-api.onrender.com/banners"
+    "http://localhost:4000/banners"
   );
 
   return (
     <>
       <HeroSection />
-      <Container
+      {/* <Container
         className="!px:w-full !px-3/4"
         sx={{
           backgroundColor: "var(--color-neutral-200)",
@@ -70,29 +68,51 @@ export default async function HomePage() {
               </Typography>
             </Box>
           ))}
-      </Container>
+      </Container> */}
       <ProductSlider sx={{ pt: "20px" }} data={dataShirts} />
-      <Container>
-        <Container>
-          <Box className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-4 px-8 md:px-16 py-4 md:py-8">
-            <CardMedia component="img" className="h-full rounded-2xl" />
-          </Box>
-        </Container>
-      </Container>
+      {/* <Container>
+        <Box className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-4 px-8 md:px-16 py-4 md:py-8">
+          <CardMedia
+            component="img"
+            image={"/images/hero03.jpg"}
+            alt="test1"
+            className="h-full rounded-2xl"
+          />
+          <CardMedia
+            component="img"
+            image={"/images/hero04.jpg"}
+            alt="test2"
+            className="h-full rounded-2xl"
+          />
+          <CardMedia
+            component="img"
+            image={"/images/hero05.jpg"}
+            alt="test3"
+            className="h-full rounded-2xl"
+          />
+          <CardMedia
+            component="img"
+            image={"/images/hero06.jpg"}
+            alt="test4"
+            className="h-full rounded-2xl"
+          />
+        </Box>
+      </Container> */}
       <ProductSlider sx={{ pt: "20px" }} data={dataCaps} />
       <ProductSlider sx={{ pt: "20px" }} data={dataPants} />
-      {landingBaner[0]?.src && (
+      {/* <Container sx={{ width: "100%", height: "200px", pt: "20px" }}>
         <CardMedia
+          sx={{
+            borderRadius: "12px",
+            overflow: "hidden",
+            width: "100%",
+            height: "100%",
+          }}
           component="img"
           image={landingBaner[0].src}
           alt={landingBaner[0].alt}
-          sx={{
-            width: "40px",
-            height: "auto",
-            borderRadius: "12px",
-          }}
         />
-      )}
+      </Container> */}
       <ProductSlider sx={{ pt: "20px" }} data={dataSets} />
     </>
   );
