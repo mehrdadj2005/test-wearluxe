3a64ac834b197fb47c684227bf442d95c737abea
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-07-04 10:19:15 +0330

    Update API endpoints to production URLs and clean up unused code in HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,22 @@ import { Box, CardMedia, Typography } from "@mui/material";
 
 export default async function HomePage() {
   const { data: dataShirts } = await getProduct<IProduct[]>(
-    "http://localhost:4000/products?categoryId=1"
+    "https://wearluxe-api.onrender.com/products?categoryId=1"
   );
   const { data: dataPants } = await getProduct<IProduct[]>(
-    "http://localhost:4000/products?categoryId=2"
+    "https://wearluxe-api.onrender.com/products?categoryId=2"
   );
   const { data: dataCaps } = await getProduct<IProduct[]>(
-    "http://localhost:4000/products?categoryId=3"
+    "https://wearluxe-api.onrender.com/products?categoryId=3"
   );
   const { data: dataSets } = await getProduct<IProduct[]>(
-    "http://localhost:4000/products?categoryId=4"
+    "https://wearluxe-api.onrender.com/products?categoryId=4"
   );
   const { data: dataOptions } = await getProduct<IOption[]>(
-    "http://localhost:4000/options"
+    "https://wearluxe-api.onrender.com/options"
   );
   const { data: landingBaner } = await getProduct<IBanner[]>(
-    "http://localhost:4000/banners"
+    "https://wearluxe-api.onrender.com/banners"
   );
 
   return (
@@ -73,48 +73,26 @@ export default async function HomePage() {
       </Container>
       <ProductSlider sx={{ pt: "20px" }} data={dataShirts} />
       <Container>
-        <Box className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-4 px-8 md:px-16 py-4 md:py-8">
-          <CardMedia
-            component="img"
-            image={"/images/hero03.jpg"}
-            alt="test1"
-            className="h-full rounded-2xl"
-          />
-          <CardMedia
-            component="img"
-            image={"/images/hero04.jpg"}
-            alt="test2"
-            className="h-full rounded-2xl"
-          />
-          <CardMedia
-            component="img"
-            image={"/images/hero05.jpg"}
-            alt="test3"
-            className="h-full rounded-2xl"
-          />
-          <CardMedia
-            component="img"
-            image={"/images/hero06.jpg"}
-            alt="test4"
-            className="h-full rounded-2xl"
-          />
-        </Box>
+        <Container>
+          <Box className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-4 px-8 md:px-16 py-4 md:py-8">
+            <CardMedia component="img" className="h-full rounded-2xl" />
+          </Box>
+        </Container>
       </Container>
       <ProductSlider sx={{ pt: "20px" }} data={dataCaps} />
       <ProductSlider sx={{ pt: "20px" }} data={dataPants} />
-      <Container sx={{ width: "100%", height: "200px", pt: "20px" }}>
+      {landingBaner[0]?.src && (
         <CardMedia
-          sx={{
-            borderRadius: "12px",
-            overflow: "hidden",
-            width: "100%",
-            height: "100%",
-          }}
           component="img"
           image={landingBaner[0].src}
           alt={landingBaner[0].alt}
+          sx={{
+            width: "40px",
+            height: "auto",
+            borderRadius: "12px",
+          }}
         />
-      </Container>
+      )}
       <ProductSlider sx={{ pt: "20px" }} data={dataSets} />
     </>
   );
