985ab043d7525b544f9e41791931a6b8f2ac62dc
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-07-07 13:40:11 +0330

    Refactor HomePage and Asd components to streamline product fetching and improve rendering logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,117 +1,11 @@
 import Asd from "@/components/asd";
 import HeroSection from "@/components/layout/hero";
-import ProductSlider from "@/components/products/productSlider";
-import { getProduct } from "@/services/getProduct";
-import { IBanner } from "@/types/banners";
-import { ICategory } from "@/types/category";
-import { IOption } from "@/types/options";
-import { IProduct } from "@/types/product";
-import { Box, CardMedia, Container, Typography } from "@mui/material";
 
 export default async function HomePage() {
-  const { data: dataShirts = [] } = await getProduct<IProduct[]>(
-    "/products?categoryId=1"
-  );
-  const { data: dataPants = [] } = await getProduct<IProduct[]>(
-    "/products?categoryId=2"
-  );
-  const { data: dataCaps = [] } = await getProduct<IProduct[]>(
-    "/products?categoryId=3"
-  );
-  const { data: dataSets = [] } = await getProduct<IProduct[]>(
-    "/products?categoryId=4"
-  );
-  const { data: dataOptions = [] } = await getProduct<IOption[]>("/options");
-  const { data: landingBaner = [] } = await getProduct<IBanner[]>("/banners");
-  const { data: categories } = await getProduct<ICategory[]>("/categories");
-
   return (
     <>
       <HeroSection />
       <Asd />
-      {Array.isArray(categories) &&
-        categories.map((item) => (
-          <div key={item.id}>
-            <h3>{item.slug}</h3>
-            <h3>{item.title}</h3>
-          </div>
-        ))}
-
-      {/* آپشن‌ها */}
-      {dataOptions.length > 0 && (
-        <Container
-          className="!px:w-full !px-3/4"
-          sx={{
-            backgroundColor: "var(--color-neutral-200)",
-            borderRadius: "18px",
-            display: "flex",
-            padding: "30px 0",
-            justifyContent: "space-around",
-            flexWrap: "wrap",
-          }}
-        >
-          {dataOptions.map((option) => (
-            <Box
-              key={option.id}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "row",
-                gap: "8px",
-              }}
-            >
-              <CardMedia
-                component="img"
-                image={option.src}
-                alt={option.name}
-                sx={{
-                  width: "40px",
-                  height: "auto",
-                  borderRadius: "12px",
-                }}
-              />
-              <Typography
-                variant="h6"
-                sx={{ color: "var(--color-neutral-600)" }}
-              >
-                {option.name}
-              </Typography>
-            </Box>
-          ))}
-        </Container>
-      )}
-
-      {/* لیست محصولات */}
-      {dataShirts.length > 0 && (
-        <ProductSlider sx={{ pt: "20px" }} data={dataShirts} />
-      )}
-      {dataCaps.length > 0 && (
-        <ProductSlider sx={{ pt: "20px" }} data={dataCaps} />
-      )}
-      {dataPants.length > 0 && (
-        <ProductSlider sx={{ pt: "20px" }} data={dataPants} />
-      )}
-
-      {/* بنر وسطی */}
-      {landingBaner?.[0] && (
-        <Container sx={{ width: "100%", height: "200px", pt: "20px" }}>
-          <CardMedia
-            sx={{
-              borderRadius: "12px",
-              overflow: "hidden",
-              width: "100%",
-              height: "100%",
-            }}
-            component="img"
-            image={landingBaner[0].src}
-            alt={landingBaner[0].alt}
-          />
-        </Container>
-      )}
-
-      {dataSets.length > 0 && (
-        <ProductSlider sx={{ pt: "20px" }} data={dataSets} />
-      )}
     </>
   );
 }
--- src/components/asd.tsx
+++ src/components/asd.tsx
@@ -1,27 +1,103 @@
-"use client";
 import { getProduct } from "@/services/getProduct";
-import { ICategory } from "@/types/category";
-import { useEffect, useState } from "react";
+import { IBanner } from "@/types/banners";
+import { IOption } from "@/types/options";
+import { IProduct } from "@/types/product";
 
-export default function Asd() {
-  const [category, setCategory] = useState<ICategory[]>([]);
+import ProductSlider from "@/components/products/productSlider";
+import { Box, CardMedia, Container, Typography } from "@mui/material";
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const { data } = await getProduct<ICategory[]>("/categories");
-      setCategory(data);
-    };
-    fetchCategories();
-  }, []);
+export default async function Asd() {
+  const { data: dataShirts = [] } = await getProduct<IProduct[]>(
+    "/products?categoryId=1"
+  );
+  const { data: dataPants = [] } = await getProduct<IProduct[]>(
+    "/products?categoryId=2"
+  );
+  const { data: dataCaps = [] } = await getProduct<IProduct[]>(
+    "/products?categoryId=3"
+  );
+  const { data: dataSets = [] } = await getProduct<IProduct[]>(
+    "/products?categoryId=4"
+  );
+  const { data: dataOptions = [] } = await getProduct<IOption[]>("/options");
+  const { data: landingBaner = [] } = await getProduct<IBanner[]>("/banners");
 
   return (
-    <div>
-      <h1>Categories</h1>
-      <ul>
-        {category.map((cat) => (
-          <li key={cat.id}>{cat.title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {dataOptions.length > 0 && (
+        <Container
+          className="!px:w-full !px-3/4"
+          sx={{
+            backgroundColor: "var(--color-neutral-200)",
+            borderRadius: "18px",
+            display: "flex",
+            padding: "30px 0",
+            justifyContent: "space-around",
+            flexWrap: "wrap",
+          }}
+        >
+          {dataOptions.map((option) => (
+            <Box
+              key={option.id}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "8px",
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={option.src}
+                alt={option.name}
+                sx={{
+                  width: "40px",
+                  height: "auto",
+                  borderRadius: "12px",
+                }}
+              />
+              <Typography
+                variant="h6"
+                sx={{ color: "var(--color-neutral-600)" }}
+              >
+                {option.name}
+              </Typography>
+            </Box>
+          ))}
+        </Container>
+      )}
+
+      {/* لیست محصولات */}
+      {dataShirts.length > 0 && (
+        <ProductSlider sx={{ pt: "20px" }} data={dataShirts} />
+      )}
+      {dataCaps.length > 0 && (
+        <ProductSlider sx={{ pt: "20px" }} data={dataCaps} />
+      )}
+      {dataPants.length > 0 && (
+        <ProductSlider sx={{ pt: "20px" }} data={dataPants} />
+      )}
+
+      {/* بنر وسطی */}
+      {landingBaner?.[0] && (
+        <Container sx={{ width: "100%", height: "200px", pt: "20px" }}>
+          <CardMedia
+            sx={{
+              borderRadius: "12px",
+              overflow: "hidden",
+              width: "100%",
+              height: "100%",
+            }}
+            component="img"
+            image={landingBaner[0].src}
+            alt={landingBaner[0].alt}
+          />
+        </Container>
+      )}
+
+      {dataSets.length > 0 && (
+        <ProductSlider sx={{ pt: "20px" }} data={dataSets} />
+      )}
+    </>
   );
 }
