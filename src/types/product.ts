f7e77362ba197a8f8eb24e0493f07b08b686ee95
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-07-08 16:48:11 +0330

    .

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,31 +1,58 @@
-export interface IProduct {
+// export interface IProduct {
+//   id: string;
+//   categoryId: string;
+//   categoryName: string;
+//   slug: string;
+//   name: string;
+//   price: number;
+//   discountedPercentage: number;
+//   discountedPrice: number;
+//   stock: boolean;
+//   images: string[];
+//   rating: number;
+//   description: string;
+//   color: string;
+//   colors: string[];
+//   material: string;
+//   publishTime: string;
+//   publishTimeSort: string;
+//   sales: number;
+//   size: string;
+//   sizes: {
+//     [key: string]: {
+//       dimensions: {
+//         height: number;
+//         width: number;
+//       };
+//       stock: boolean;
+//     };
+//   };
+//   type: string;
+// }
+
+type Product = {
   id: string;
   categoryId: string;
   categoryName: string;
-  slug: string;
-  name: string;
-  price: number;
-  discountedPercentage: number;
-  discountedPrice: number;
-  stock: boolean;
-  images: string[];
-  rating: number;
-  description: string;
   color: string;
   colors: string[];
+  description: string;
+  discountedPercentage: number;
+  discountedPrice: number;
+  images: string[];
   material: string;
+  name: string;
+  price: number;
   publishTime: string;
   publishTimeSort: string;
+  rating: number;
   sales: number;
   size: string;
-  sizes: {
-    [key: string]: {
-      dimensions: {
-        height: number;
-        width: number;
-      };
-      stock: boolean;
-    };
-  };
+  sizes: Record<
+    string,
+    { dimensions: { height: number; width: number }; stock: boolean }
+  >;
+  slug: string;
+  stock: boolean;
   type: string;
-}
+};
