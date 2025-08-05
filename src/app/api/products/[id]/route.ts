4704607b645060ce4d4d69b7698e5b26a5d01eb2
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-08-05 18:07:55 +0330

    .

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,3 +1,4 @@
+import { IProduct } from "@/types/product";
 import { promises as fs } from "fs";
 import { NextRequest, NextResponse } from "next/server";
 import path from "path";
@@ -11,7 +12,7 @@ export async function GET(request: NextRequest) {
     const file = await fs.readFile(filePath, "utf-8");
     const json = JSON.parse(file);
 
-    const product = json.products.find((p: any) => p.id === id);
+    const product = json.products.find((p: IProduct) => p.id === id);
 
     if (!product) {
       return NextResponse.json({ error: "محصول پیدا نشد" }, { status: 404 });
