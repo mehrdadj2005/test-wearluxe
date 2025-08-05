85a1136ccf07ea61efa2b1a5275811ab25aecabe
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-08-05 17:58:33 +0330

    .

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,19 +1,27 @@
-import { IProduct } from "@/types/product";
 import { promises as fs } from "fs";
 import path from "path";
 
+type Product = {
+  id: string;
+  name: string;
+  price: number;
+  // سایر فیلدهایی که نیاز داری رو اینجا اضافه کن
+};
+
 export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
+  request: Request,
+  context: { params: { id: string } }
 ) {
+  const { id } = context.params;
+
   const filePath = path.join(process.cwd(), "src/data/db.json");
   const file = await fs.readFile(filePath, "utf-8");
   const data = JSON.parse(file);
 
-  const product = data.products.find((item: IProduct) => item.id === params.id);
+  const product = data.products.find((item: Product) => item.id === id);
 
   if (!product) {
-    return new Response("محصول پیدا نشد", { status: 404 });
+    return new Response("Product not found", { status: 404 });
   }
 
   return Response.json(product);
