e102244d4c2179ebf300233dd777a10825fb7e28
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-08-05 17:34:25 +0330

    .

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,17 @@
+import { products } from "@/data/db.json";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  const product = products.find((p) => p.id === id);
+
+  if (!product) {
+    return NextResponse.json({ message: "Product not found" }, { status: 404 });
+  }
+
+  return NextResponse.json(product);
+}
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,37 +1,3 @@
-// // src/app/api/products/route.ts
-// import { promises as fs } from "fs";
-// import { NextResponse } from "next/server";
-// import path from "path";
-
-// // گرفتن categoryId از URL (مثل: /api/products?categoryId=1)
-// export async function GET(request: Request) {
-//   try {
-//     const { searchParams } = new URL(request.url);
-//     const categoryId = searchParams.get("categoryId");
-
-//     // مسیر صحیح فایل دیتا
-//     const filePath = path.join(process.cwd(), "src", "data", "db.json");
-//     const file = await fs.readFile(filePath, "utf-8");
-//     const json = JSON.parse(file);
-
-//     let filteredProducts = json.products;
-
-//     if (categoryId) {
-//       filteredProducts = filteredProducts.filter(
-//         (product: { categoryId: string | number }) =>
-//           String(product.categoryId) === categoryId
-//       );
-//     }
-
-//     return NextResponse.json(filteredProducts);
-//   } catch (err) {
-//     return NextResponse.json(
-//       { error: "خطا در دریافت محصولات" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
 import { IProduct } from "@/types/product";
 import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
